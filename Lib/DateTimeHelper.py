7c6bd6133740a38f9547862fe6eb7395f4a7ed1d
staymanHou <hhyycc0418@gmail.com>
2013-09-25 17:56:11 -0400

    add datetimehelper

---
+++ Lib/DateTimeHelper.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+
+def floorbyhour(input_datetime):
+    d = timedelta(seconds=(input_datetime.minute*60+input_datetime.second),microseconds=input_datetime.microsecond)
+    return input_datetime-d
+
+def floorbyday(input_datetime, hour_shift=0):
+    time_shift = timedelta(seconds=hour_shift*360)
+    input_datetime -= time_shift
+    d = timedelta(seconds=(input_datetime.hour*3600+input_datetime.minute*60+input_datetime.second),microseconds=input_datetime.microsecond)
+    input_datetime += time_shift
+    return input_datetime-d
+
+def timetoclock(cal_time):
+    return int(cal_time/3600)%24
