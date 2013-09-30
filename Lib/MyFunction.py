f95e1076c57b4e55919c8974759c98fda90ef7b4
staymanHou <hhyycc0418@gmail.com>
2013-09-30 09:31:06 -0400

    Generator seems finished

---
+++ Lib/MyFunction.py
@@ -0,0 +1,46 @@
+import os
+
+def file2list(file_path,NoDup=True):
+    mylst = []
+    if not os.path.exists(file_path):
+        open(file_path,'w')
+    lines = open(file_path).readlines()
+    for line in lines:
+        if line.strip()!='':
+            mylst.append(line.strip())
+    if NoDup:
+        temp_dict = {}
+        for item in mylst:
+            temp_dict[item] = 0
+        mylst = []
+        for key in temp_dict.keys():
+            mylst.append(key)
+    return mylst
+
+def randpick(pick_num,orig_list):
+    from random import sample
+    return sample(orig_list, pick_num)
+
+def Username2Userid(l_username):
+    import requests
+    import re
+    import bsddb
+    db = bsddb.hashopen('name2id.db')
+    if l_username in db:
+        if db[l_username][0] == '-':
+            return abs(int(db[l_username]))
+        else: 
+            return unicode(db[l_username])
+    l_url = 'https://twitter.com/'+l_username
+    l_r = requests.get(l_url)
+    m = re.search('<div class="profile-card-inner" data-screen-name=".+?" data-user-id=".+?">',l_r.text)
+    if m == None:
+        db[l_username] = str(-l_r.status_code)
+        return l_r.status_code
+    m = re.search('data-user-id=".+?"',m.group(0))
+    m = m.group(0)[14:-1]
+    if len(m)>0:
+        db[l_username] = m
+        return m
+    db[l_username] = str(-l_r.status_code)
+    return l_r.status_code
--- Lib/TwitterAdGenerator.py
+++ Lib/TwitterAdGenerator.py
@@ -1,8 +1,8 @@
 from MyDict import COUNTRY_ID_DICT,INTEREST_ID_DICT
 from MyFunction import Username2Userid, file2list, randpick
 from TwitterCampaign import TwitterCampaign
+from LocalStatus import LocalStatus
 from random import randint
-from Mydb import MydbGetAccConfForGnrt,MydbGetAlvAndCrtpdNum
 import os
 
 list_file_dir = 'list_files/'
@@ -11,12 +11,11 @@ intst_list_name = 'intst_list.txt'
 cntry_list_name = 'cntry_list.txt'
 
 def crtpd2refillcmp(account, generate_num):
-    generate_num = conf['MAX_CAMPAIGN_NUM'] - MydbGetAlvAndCrtpdNum(account.fi_id)
     if generate_num<=0: return None
-    prefix = '%s_'%conf['USERNAME']
-    conf['USER_PRIVATE_WEIGHT'] = min(1,max(0,conf['USER_PRIVATE_WEIGHT']))
-    conf['INTST_PRIVATE_WEIGHT'] = min(1,max(0,conf['INTST_PRIVATE_WEIGHT']))
-    conf['CNTRY_PRIVATE_WEIGHT'] = min(1,max(0,conf['CNTRY_PRIVATE_WEIGHT']))
+    prefix = '%s_'%account.username
+    account.user_private_weight = min(1,max(0,account.user_private_weight))
+    account.intst_private_weight = min(1,max(0,account.intst_private_weight))
+    account.cntry_private_weight = min(1,max(0,account.cntry_private_weight))
     if not os.path.exists(list_file_dir):
         os.makedirs(list_file_dir)
     user_common_raw_list = file2list(list_file_dir+'common_'+user_list_name)
@@ -27,16 +26,16 @@ def crtpd2refillcmp(account, generate_num):
     cntry_private_raw_list = file2list(list_file_dir+prefix+cntry_list_name)
     for i in range(generate_num):
         ####### randomly choose intst_num, user_num, cntry_num, bid
-        user_num = randint(conf['USER_NUM_LOW'],conf['USER_NUM_HIGH'])
-        user_private_num = int(user_num*conf['USER_PRIVATE_WEIGHT'])
+        user_num = randint(account.user_num_low, account.user_num_high)
+        user_private_num = int(user_num*account.user_private_weight)
         user_common_num = user_num - user_private_num
-        intst_num = randint(conf['INTST_NUM_LOW'],conf['INTST_NUM_HIGH'])
-        intst_private_num = int(intst_num*conf['INTST_PRIVATE_WEIGHT'])
+        intst_num = randint(account.intst_num_low, account.intst_num_high)
+        intst_private_num = int(intst_num*account.intst_private_weight)
         intst_common_num = intst_num - intst_private_num
-        cntry_num = randint(conf['CNTRY_NUM_LOW'],conf['CNTRY_NUM_HIGH'])
-        cntry_private_num = int(cntry_num*conf['CNTRY_PRIVATE_WEIGHT'])
+        cntry_num = randint(account.cntry_num_low, account.cntry_num_high)
+        cntry_private_num = int(cntry_num*account.cntry_private_weight)
         cntry_common_num = cntry_num - cntry_private_num
-        bid = float(randint(int(conf['BID_LOW']*100),int(conf['BID_HIGH']*100)))/100
+        bid = float(randint(int(account.bid_low*100),int(account.bid_high*100)))/100
         ####### vvv generate user field vvv ########
         title_user = ''
         targeted_users = ''
@@ -123,129 +122,18 @@ def crtpd2refillcmp(account, generate_num):
         locations = locations[:-1]
         ######### vvvv create pending in db vvvv ########
         campaign = TwitterCampaign()
-        campaign['name'] = title_interest+'_'+title_user+'_'+title_country
-        campaign['fi_id'] = account.fi_id
-        campaign['total_budget'] = conf['CMP_BUDGET']
-        campaign['daily_budget'] = conf['DLY_BUDGET']
-        campaign['max_bid'] = bid
-        campaign['data'] = {'spend':[],'total_spend':0,'impressions':[],'total_impressions':0,'engagements':[],'total_engagements':0}
-        campaign['targeted_users'] = targeted_users
-        campaign['targeted_interests'] = targeted_interests
-        campaign['locations'] = locations
-        campaign['pac_to_similar'] = conf['PTS']
-        campaign['gender'] = conf['GENDER']
-        campaign['accelerated_delivery'] = conf['ACCELERATED_DELIVERY']
-        campaign.save(0)
+        campaign.name = title_interest+'_'+title_user+'_'+title_country
+        campaign.local_status = LocalStatus.TitletoPK['CreatePending']
+        campaign.fi_id = account.fi_id
+        campaign.total_budget = account.cmp_budget
+        campaign.daily_budget = account.daily_budget
+        campaign.max_bid = bid
+        campaign.data = {'spend':[],'total_spend':0,'impressions':[],'total_impressions':0,'engagements':[],'total_engagements':0}
+        campaign.targeted_users = targeted_users
+        campaign.targeted_interests = targeted_interests
+        campaign.locations = locations
+        campaign.pac_to_similar = account.pts
+        campaign.gender = account.gender
+        campaign.accelerated_delivery = account.accelerated_delivery
+        campaign.save()
     return generate_num
-
-def randomlygeneratecreatepending(fi_id,generate_num=1,intst_num_low=0,intst_num_high=0,user_num_low=0,user_num_high=0,cntry_num_low=0,cntry_num_high=0,bid_low=0.01,bid_high=0.01,cmp_budget=100,dly_budget=10,pts='on',gender=0,ac=True):
-    if generate_num<=0:
-        raise 'generate_num should at least be 1'
-    print 'Generator start'
-    create_counter = 0
-    for i in range(generate_num):
-        ####### vvv generate user field vvv ########
-        title_user = ''
-        targeted_users = ''
-        pick_list = []
-            ####### randomly choose intst_num, user_num, cntry_num, bid
-        intst_num = randint(intst_num_low,intst_num_high)
-        user_num = randint(user_num_low,user_num_high)
-        cntry_num = randint(cntry_num_low,cntry_num_high)
-        bid = float(randint(int(bid_low*100),int(bid_high*100)))/100       
-        
-        raw_list = file2list('target_user_list_file.txt')
-        if len(raw_list)>user_num:
-            pick_list = randpick(user_num,raw_list)
-        else:
-            pick_list = raw_list
-        if len(pick_list)==0:
-            title_user = 'NoUser'
-        elif len(pick_list)==1:
-            title_user = pick_list[0]
-        else:
-            title_user = 'Mltuser_'+pick_list[0]
-        title_user = title_user[:15]
-        target_user_id = []
-        for username in pick_list:
-            t_userid = Username2Userid(username)
-            if type(t_userid) != unicode:
-                continue
-            target_user_id.append(t_userid)
-        for t_userid in target_user_id:
-            targeted_users += t_userid+','
-        targeted_users = targeted_users[:-1]
-        ####### vvv generate interest field vvv ########
-        title_interest = ''
-        targeted_interests = ''
-        pick_list = []
-        raw_list = file2list('interest_list_file.txt')
-        if len(raw_list)>intst_num:
-            pick_list = randpick(intst_num,raw_list)
-        else:
-            pick_list = raw_list
-        if len(pick_list)==0:
-            title_interest = 'NoIntst'
-        elif len(pick_list)==1:
-            title_interest = pick_list[0]
-        else:
-            title_interest = 'Mltintst_'+pick_list[0]
-        title_interest = title_interest[:15]
-        target_interest_id = []
-        for interest in pick_list:
-            if interest.lower() not in INTEREST_ID_DICT:
-                continue
-            t_intstid = INTEREST_ID_DICT[interest.lower()]
-            target_interest_id.append(t_intstid)
-        for t_intstid in target_interest_id:
-            targeted_interests += t_intstid+','
-        targeted_interests = targeted_interests[:-1]
-        ####### vvv generate location field vvv ########
-        title_country = ''
-        locations = ''
-        pick_list = []
-        raw_list = file2list('country_list_file.txt')
-        if len(raw_list)>cntry_num:
-            pick_list = randpick(cntry_num,raw_list)
-        else:
-            pick_list = raw_list
-        if len(pick_list)==0:
-            title_country = 'Worldwide'
-        elif len(pick_list)==1:
-            title_country = pick_list[0]
-        else:
-            title_country = 'Mltcntry_'+pick_list[0]
-        title_country = title_country[:15]
-        target_location_id = []
-        for location in pick_list:
-            if location.lower() not in COUNTRY_ID_DICT:
-                continue
-            t_cntryid = COUNTRY_ID_DICT[location.lower()]
-            target_location_id.append(t_cntryid)
-        for t_cntryid in target_location_id:
-            locations += t_cntryid+','
-        locations = locations[:-1]
-    
-        ######### vvvv create pending in db vvvv ########
-
-        campaign = TwitterCampaign()
-        campaign['name'] = title_interest+'_'+title_user+'_'+title_country
-        campaign['fi_id'] = str(account.fi_id)
-        campaign['total_budget'] = str(cmp_budget)
-        campaign['daily_budget'] = str(dly_budget)
-        campaign['max_bid'] = str(bid)
-        campaign['data'] = {'spend':[],'total_spend':0,'impressions':[],'total_impressions':0,'engagements':[],'total_engagements':0}
-        campaign['targeted_users'] = targeted_users
-        campaign['targeted_interests'] = targeted_interests
-        campaign['locations'] = locations
-        campaign['pac_to_similar'] = pts
-        campaign['gender'] = str(gender)
-        campaign['accelerated_delivery'] = str(ac).lower()
-
-        campaign.save(0)
-
-        create_counter += 1
-
-    print 'Generator finished. Totally %d campaign(s) have been put into create pending.'%create_counter
-    return
-
